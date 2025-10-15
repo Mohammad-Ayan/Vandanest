1498f074d041c11f8f9349a3da5a548210cf3553
Mohammad Ayan <ayan2015786@gmail.com>
2025-10-15 23:59:17 +0530

    refactor: enhance listing forms and views with Bootstrap styling and improved layout

--- models/listing.js
+++ models/listing.js
@@ -2,29 +2,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title: {
-        type: String,
-        required: true,
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    filename: String,
+    url: {
+      type: String,
+      required: true,
+      default:
+        "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
     },
-    description: String,
-    price: Number,
-    location: String,
-    image: {
-        type: [
-            {
-                filename: String,
-                url: String,
-            }
-        ],
-        default: [
-            {
-                filename: "listingimage",
-                url: "https://unsplash.com/illustrations/a-blue-square-with-a-white-check-mark-on-it-PB3ZserDqcs"
-            }
-        ],
-    }, 
-    country: String,
-    // availableDates: [Date],
+  },
+  price: Number,
+  location: String,
+  country: String,
 });
 
 const Listing = mongoose.model('Listing', listingSchema);
